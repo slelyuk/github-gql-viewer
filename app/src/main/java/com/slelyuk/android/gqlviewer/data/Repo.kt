87c5bbb282a3b1962d67a58289399880da6f5878
Stas Lelyuk <s.leliyk@gmail.com>
2018-04-11 11:39:03 +0300

    Add input values to graphql query instead of hardcoded ones

--- app/src/main/java/com/slelyuk/android/gqlviewer/data/Repo.kt
+++ app/src/main/java/com/slelyuk/android/gqlviewer/data/Repo.kt
@@ -3,6 +3,8 @@ package com.slelyuk.android.gqlviewer.data
 import com.slelyuk.android.gqlviewer.PublicRepositoriesQuery
 
 data class Repo(
+    val id: String,
+    val url: String,
     val name: String,
     val description: String?,
     val forksCount: Long?,
@@ -12,7 +14,9 @@ fun PublicRepositoriesQuery.Data.toRepos(): List<Repo> {
   val edges = this.search().edges()
   return edges?.map { it.node()?.fragments()?.repositoryItem() }.orEmpty()
       .map {
-        Repo(it?.nameWithOwner().orEmpty(),
+        Repo(it?.id().orEmpty(),
+            it?.url().toString(),
+            it?.nameWithOwner().orEmpty(),
             it?.description(),
             it?.forkCount(),
             it?.stargazers()?.totalCount())
